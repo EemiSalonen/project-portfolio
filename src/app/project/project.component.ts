a9e27b89988539524d253788a26b88f02a707e6a
Eemi Salonen <ab6905@student.jamk.fi>
2023-10-20 20:00:56 +0300

    issues added to drop down list

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -4,30 +4,40 @@ import { ActivatedRoute } from "@angular/router";
 import { projects } from "../projects/projects";
 
 @Component({
-	selector: "app-project",
-	templateUrl: "./project.component.html",
-	styleUrls: ["./project.component.css"],
+  selector: "app-project",
+  templateUrl: "./project.component.html",
+  styleUrls: ["./project.component.css"],
 })
 export class ProjectComponent {
-	@Input() title!: string;
+  @Input() title!: string;
 
-	constructor(private route: ActivatedRoute) {
-		this.getProject();
-	}
+  open = "";
 
-	public placeholder: Project = {
-		id: -1,
-		title: "placeholder",
-		desc: "placeholder",
-		issues: [],
-	};
+  constructor(private route: ActivatedRoute) {
+    this.getProject();
+  }
 
-	public proj!: Project;
+  public placeholder: Project = {
+    id: -1,
+    title: "placeholder",
+    desc: "placeholder",
+    issues: [],
+  };
 
-	public getProject() {
-		let param: number;
-		this.route.params.subscribe((params) => (param = Number(params["id"])));
+  public proj!: Project;
 
-		this.proj = projects.find((proj) => proj.id === param) || this.placeholder;
-	}
+  public getProject() {
+    let param: number;
+    this.route.params.subscribe((params) => (param = Number(params["id"])));
+
+    this.proj = projects.find((proj) => proj.id === param) || this.placeholder;
+  }
+
+  public openIssue(title: string) {
+    if (this.open === title) {
+      this.open = "";
+    } else {
+      this.open = title;
+    }
+  }
 }
--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -1,34 +1,52 @@
 import { Project } from "../types/Project";
 
 export const projects: Project[] = [
-	{
-		id: 0,
-		title: "Tournament Assembly",
-		desc: "This project is part of the Ticorporate course in JAMK. During the coursestudents do a larger project of their own choosing in groups of five. My role in the project was product owner, technical supervisor and testing. I wrote code in the frontend aswell as the backend and made sure the project was on time and retained the vision we planned in the beginning.",
-		issues: [
-			{
-				title: "Tournament caching",
-				desc: "App should save latest tournament for later use",
-				resolution:
-					"To solve this issue I used localStorage to store tournament data as a string using a tokenification function. After the string has been created it can be stored in localStorage and loaded when needed with the detokenification function that returns the string back to usable format.",
-				images: [
-					{
-						src: "../../assets/tokenify.PNG",
-						alt: "tokenify",
-						desc: "Creating serialized token",
-					},
-					{
-						src: "../../assets/detokenify.PNG",
-						alt: "detokenify",
-						desc: "Return token to usable object",
-					},
-					{
-						src: "../../assets/url.PNG",
-						alt: "token in url",
-						desc: "Tournament token in URL",
-					},
-				],
-			},
-		],
-	},
+  {
+    id: 0,
+    title: "Tournament Assembly",
+    desc: "This project is part of the Ticorporate course in JAMK. During the coursestudents do a larger project of their own choosing in groups of five. My role in the project was product owner, technical supervisor and testing. I wrote code in the frontend aswell as the backend and made sure the project was on time and retained the vision we planned in the beginning.",
+    issues: [
+      {
+        title: "Tournament caching",
+        desc: "App should save latest tournament for later use.",
+        resolution:
+          "To solve this issue I used localStorage to store tournament data as a string using a tokenification function. After the string has been created it can be stored in localStorage and loaded when needed with the detokenification function that returns the string back to usable format.",
+        images: [
+          {
+            src: "../../assets/tokenify.PNG",
+            alt: "tokenify",
+            desc: "Creating serialized token",
+          },
+          {
+            src: "../../assets/detokenify.PNG",
+            alt: "detokenify",
+            desc: "Return token to usable object",
+          },
+          {
+            src: "../../assets/url.PNG",
+            alt: "token in url",
+            desc: "Tournament token in URL",
+          },
+        ],
+      },
+      {
+        title: "Playoff bracket algorithm",
+        desc: "Playoffs bracket needs to set up matches, resolve matches and declare a winner.",
+        resolution:
+          "Issue was solved by creating a matrix with arrays, that has each round simulated as an array which has the matches listed as objects. For a deeper learning experience, I decided to built the system without using any external libraries.",
+        images: [
+          {
+            src: "../../assets/matrix.PNG",
+            desc: "Matrix for rendering and resolving",
+            alt: "matrix",
+          },
+          {
+            src: "../../assets/playoff_bracket.PNG",
+            desc: "Playoff brackets rendered",
+            alt: "playoff bracket",
+          },
+        ],
+      },
+    ],
+  },
 ];
