8fd425526e83fa4428f29b11da07b95f0a2735fd
Eemi Salonen <ab6905@student.jamk.fi>
2023-10-24 10:51:42 +0300

    open project button added

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,16 +1,25 @@
 import { Component, Input } from "@angular/core";
 import { Icon } from "../types/Icon";
+import { Project } from "../types/Project";
+import { Router } from "@angular/router";
 
 @Component({
-	selector: "app-card",
-	templateUrl: "./card.component.html",
-	styleUrls: ["./card.component.css"],
+  selector: "app-card",
+  templateUrl: "./card.component.html",
+  styleUrls: ["./card.component.css"],
 })
 export class CardComponent {
-	@Input() title!: string;
-	@Input() imgsrc!: string;
-	@Input() minDesc!: string;
-	@Input() desc!: string;
-	@Input() ghLink!: string;
-	@Input() icons!: Icon[];
+  constructor(private router: Router) {}
+
+  @Input() title!: string;
+  @Input() imgsrc!: string;
+  @Input() minDesc!: string;
+  @Input() desc!: string;
+  @Input() ghLink!: string;
+  @Input() icons!: Icon[];
+  @Input() projectId!: number;
+
+  openProject(id: number) {
+    this.router.navigateByUrl(`/project/${id}`);
+  }
 }
