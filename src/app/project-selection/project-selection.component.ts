54d803e8be17f50134eb22a431ace40c453cb2e3
EemiSalonen <eemi.salonen@gmail.com>
2023-10-25 20:55:10 +0300

    background image added and project animations

--- src/app/project-selection/project-selection.component.ts
+++ src/app/project-selection/project-selection.component.ts
@@ -1,17 +1,11 @@
 import { Component } from "@angular/core";
-import { Router } from "@angular/router";
 
 @Component({
-	selector: "app-landing",
-	templateUrl: "./landing.component.html",
-	styleUrls: ["./landing.component.css"],
+	selector: "app-project-selection",
+	templateUrl: "./project-selection.component.html",
+	styleUrls: ["./project-selection.component.css"],
 })
-export class LandingComponent {
-	constructor(private router: Router) {}
-
-	public projectsPath = "/projects";
-	public contactPath = "/contact";
-
+export class ProjectSelectionComponent {
 	public tourasIcons = [
 		{
 			iconSrc:
@@ -47,8 +41,4 @@ export class LandingComponent {
 			alt: "mongooselogo",
 		},
 	];
-
-	public navigate(path: string) {
-		this.router.navigateByUrl(path);
-	}
 }
